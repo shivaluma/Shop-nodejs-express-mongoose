3be1fe34eadae85e92c79bce2d76b21d46445d5d
shivaluma <shivaluma@gmail.com>
2019-11-19 19:50:41 +0700

    Finish filter item

--- controllers/product.js
+++ controllers/product.js
@@ -30,14 +30,15 @@ exports.getProduct = (req, res, next) => {
 
 exports.getProducts = (req, res, next) => {
     const ptype = req.query.type !== undefined ? req.query.type : '';
-    const pprice = req.query.price !== undefined ? req.query.price : '';
+    const pprice = req.query.price !== undefined ? req.query.price : 999;
     const psize = req.query.size !== undefined ? req.query.size : '';
     const plabel = req.query.label !== undefined ? req.query.label : '';
     const plowerprice = pprice !== 999 ? pprice - 50 : 0;
     Products.find({
         type: new RegExp(ptype, 'i'),
         size: new RegExp(psize, 'i'),
-        labels: new RegExp(plabel, 'i')
+        labels: new RegExp(plabel, 'i'),
+        price: { $gt: plowerprice, $lt: pprice }
     })
         .then(products => {
             res.render('products', {
