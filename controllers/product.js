6f886ab28c38810ed3ea4737b17aa27fe5f78370
unknown <tan.thai.2t@gmail.com>
2019-11-29 12:38:42 +0700

    Update index view, render login page

--- controllers/product.js
+++ controllers/product.js
@@ -1,52 +1,52 @@
-const Products = require('../models/product');
+const Products = require("../models/product");
 
 // demo chu chua xu ly
 exports.getIndexProducts = (req, res, next) => {
-    Products.find()
-        .then(products => {
-            res.render('index', {
-                title: 'Trang chủ',
-                trendings: products
-            });
-        })
-        .catch(err => {
-            console.log(err);
-        });
+  Products.find()
+    .then(products => {
+      res.render("index", {
+        title: "Trang chủ",
+        trendings: products
+      });
+    })
+    .catch(err => {
+      console.log(err);
+    });
 };
 
 exports.getProduct = (req, res, next) => {
-    const prodId = req.params.productId;
-    Products.find({ _id: `${prodId}` })
-        .then(product => {
-            res.render('product', {
-                title: `${product[0].name}`,
-                prod: product[0]
-            });
-        })
-        .catch(err => {
-            console.log(err);
-        });
+  const prodId = req.params.productId;
+  Products.find({ _id: `${prodId}` })
+    .then(product => {
+      res.render("product", {
+        title: `${product[0].name}`,
+        prod: product[0]
+      });
+    })
+    .catch(err => {
+      console.log(err);
+    });
 };
 
 exports.getProducts = (req, res, next) => {
-    const ptype = req.query.type !== undefined ? req.query.type : '';
-    const pprice = req.query.price !== undefined ? req.query.price : 999999;
-    const psize = req.query.size !== undefined ? req.query.size : '';
-    const plabel = req.query.label !== undefined ? req.query.label : '';
-    const plowerprice = pprice !== 999999 ? pprice - 50 : 0;
-    Products.find({
-        type: new RegExp(ptype, 'i'),
-        size: new RegExp(psize, 'i'),
-        labels: new RegExp(plabel, 'i'),
-        price: { $gt: plowerprice, $lt: pprice }
+  const ptype = req.query.type !== undefined ? req.query.type : "";
+  const pprice = req.query.price !== undefined ? req.query.price : 999999;
+  const psize = req.query.size !== undefined ? req.query.size : "";
+  const plabel = req.query.label !== undefined ? req.query.label : "";
+  const plowerprice = pprice !== 999999 ? pprice - 50 : 0;
+  Products.find({
+    type: new RegExp(ptype, "i"),
+    size: new RegExp(psize, "i"),
+    labels: new RegExp(plabel, "i"),
+    price: { $gt: plowerprice, $lt: pprice }
+  })
+    .then(products => {
+      res.render("products", {
+        title: "Danh sách sản phẩm",
+        allProducts: products
+      });
     })
-        .then(products => {
-            res.render('products', {
-                title: 'Danh sách sản phẩm',
-                allProducts: products
-            });
-        })
-        .catch(err => {
-            console.log(err);
-        });
+    .catch(err => {
+      console.log(err);
+    });
 };
