25bc1450eff5ec99dab20361e0117fd12fb74e75
shivaluma <shivaluma@gmail.com>
2019-11-30 11:54:28 +0700

    Fix app.use middleware

--- routes/auth.js
+++ routes/auth.js
@@ -6,15 +6,8 @@ const authController = require("../controllers/auth");
 router.get("/login", authController.getLogin);
 router.post("/login", authController.postLogin);
 
-router.get("/logout", isLoggedIn, authController.getLogout);
+router.get("/logout", authController.getLogout);
 router.get("/create-account", authController.getSignUp);
 router.post("/create-account", authController.postSignUp);
 
 module.exports = router;
-
-function isLoggedIn(req, res, next) {
-  // if user is authenticated in the session, carry on
-  if (req.isAuthenticated()) return next();
-  // if they aren't redirect them to the home page
-  res.redirect("/");
-}
