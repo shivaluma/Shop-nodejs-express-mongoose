696111e28cb13e0589c048ae10d824b9001afff8
unknown <tan.thai.2t@gmail.com>
2019-12-15 17:04:05 +0700

    Add account info and change account info ft

--- controllers/user.js
+++ controllers/user.js
@@ -1,1 +1,25 @@
 const Users = require("../models/user");
+
+exports.getAccount = (req, res, next) => {
+  res.render("account", {
+    title: "Thông tin tài khoản",
+    user: req.user
+  });
+};
+
+exports.getAccountChange = (req, res, next) => {
+  res.render("account-change-info", {
+    title: "Thay đổi thông tin tài khoản",
+    user: req.user
+  });
+};
+
+exports.postAccountChange = (req, res, next) => {
+  req.user.firstName = req.body.firstName;
+  req.user.lastName = req.body.lastName;
+  req.user.email = req.body.email;
+  req.user.address = req.body.address;
+  req.user.phoneNumber = req.body.phoneNumber;
+  req.user.save();
+  res.redirect("/account");
+};
--- routes/auth.js
+++ routes/auth.js
@@ -2,12 +2,22 @@ var express = require("express");
 var router = express.Router();
 var passport = require("passport");
 const authController = require("../controllers/auth");
+const userController = require("../controllers/user");
 
 router.get("/login", authController.getLogin);
+
 router.post("/login", authController.postLogin);
 
 router.get("/logout", authController.getLogout);
+
 router.get("/create-account", authController.getSignUp);
+
 router.post("/create-account", authController.postSignUp);
+
+router.get("/account", userController.getAccount);
+
+router.get("/account-change-info", userController.getAccountChange);
+
+router.post("/account-change-info", userController.postAccountChange);
 
 module.exports = router;
