7501962e222ee043e4821db48d8f5caa2dcbd277
unknown <tan.thai.2t@gmail.com>
2020-01-08 12:48:38 +0700

    fix some bug

--- controllers/product.js
+++ controllers/product.js
@@ -1,12 +1,12 @@
-const Products = require('../models/product');
-const Categories = require('../models/productCategory');
-const Cart = require('../models/cart');
-var Users = require('../models/user');
-const Order = require('../models/order');
+const Products = require("../models/product");
+const Categories = require("../models/productCategory");
+const Cart = require("../models/cart");
+var Users = require("../models/user");
+const Order = require("../models/order");
 
 var ITEM_PER_PAGE = 12;
 var SORT_ITEM;
-var sort_value = 'Giá thấp tới cao';
+var sort_value = "Giá thấp tới cao";
 var ptype;
 var ptypesub;
 var pprice = 999999;
@@ -30,10 +30,10 @@ exports.getIndexProducts = (req, res, next) => {
     .then(products => {
       Products.find()
         .limit(8)
-        .sort('buyCounts')
+        .sort("buyCounts")
         .then(products2 => {
-          res.render('index', {
-            title: 'Trang chủ',
+          res.render("index", {
+            title: "Trang chủ",
             user: req.user,
             trendings: products,
             hots: products2,
@@ -56,18 +56,20 @@ exports.getProduct = (req, res, next) => {
   }
   const prodId = req.params.productId;
   Products.findOne({ _id: `${prodId}` }).then(product => {
-    Products.find({ 'productType.main': product.productType.main }).then(relatedProducts => {
-      res.render('product', {
-        title: `${product.name}`,
-        user: req.user,
-        prod: product,
-        comments: product.comment.items,
-        allComment: product.comment.total,
-        cartProduct: cartProduct,
-        relatedProducts: relatedProducts
-      });
-      product.save();
-    });
+    Products.find({ "productType.main": product.productType.main }).then(
+      relatedProducts => {
+        res.render("product", {
+          title: `${product.name}`,
+          user: req.user,
+          prod: product,
+          comments: product.comment.items,
+          allComment: product.comment.total,
+          cartProduct: cartProduct,
+          relatedProducts: relatedProducts
+        });
+        product.save();
+      }
+    );
   });
 };
 
@@ -92,19 +94,19 @@ exports.getProducts = (req, res, next) => {
   SORT_ITEM = req.query.orderby;
 
   if (SORT_ITEM == -1) {
-    sort_value = 'Giá cao tới thấp';
-    price = '-1';
+    sort_value = "Giá cao tới thấp";
+    price = "-1";
   }
   if (SORT_ITEM == 1) {
-    sort_value = 'Giá thấp tới cao';
-    price = '1';
+    sort_value = "Giá thấp tới cao";
+    price = "1";
   }
 
   if (Object.entries(req.query).length == 0) {
-    ptype = '';
-    psize = '';
-    plabel = '';
-    ptypesub = '';
+    ptype = "";
+    psize = "";
+    plabel = "";
+    ptypesub = "";
   }
 
   var page = +req.query.page || 1;
@@ -118,38 +120,38 @@ exports.getProducts = (req, res, next) => {
 
   let childType;
   if (productType == undefined) {
-    productType = '';
+    productType = "";
   } else {
     Categories.findOne({ name: `${productType}` }, (err, data) => {
       if (err) console.log(err);
       if (data) {
-        childType = data.childName || '';
+        childType = data.childName || "";
       } else {
-        childType = '';
+        childType = "";
       }
     });
   }
 
   if (productChild == undefined) {
-    productChild = '';
+    productChild = "";
   }
 
   Products.find({
-    'productType.main': new RegExp(productType, 'i'),
-    'productType.sub': new RegExp(productChild, 'i'),
-    size: new RegExp(psize, 'i'),
+    "productType.main": new RegExp(productType, "i"),
+    "productType.sub": new RegExp(productChild, "i"),
+    size: new RegExp(psize, "i"),
     price: { $gt: plowerprice, $lt: pprice },
-    labels: new RegExp(plabel, 'i')
+    labels: new RegExp(plabel, "i")
   })
     .countDocuments()
     .then(numProduct => {
       totalItems = numProduct;
       return Products.find({
-        'productType.main': new RegExp(productType, 'i'),
-        'productType.sub': new RegExp(productChild, 'i'),
-        size: new RegExp(psize, 'i'),
+        "productType.main": new RegExp(productType, "i"),
+        "productType.sub": new RegExp(productChild, "i"),
+        size: new RegExp(psize, "i"),
         price: { $gt: plowerprice, $lt: pprice },
-        labels: new RegExp(plabel, 'i')
+        labels: new RegExp(plabel, "i")
       })
         .skip((page - 1) * ITEM_PER_PAGE)
         .limit(ITEM_PER_PAGE)
@@ -158,8 +160,8 @@ exports.getProducts = (req, res, next) => {
         });
     })
     .then(products => {
-      res.render('products', {
-        title: 'Danh sách sản phẩm',
+      res.render("products", {
+        title: "Danh sách sản phẩm",
         user: req.user,
         allProducts: products,
         currentPage: page,
@@ -184,7 +186,7 @@ exports.getProducts = (req, res, next) => {
 
 exports.postNumItems = (req, res, next) => {
   ITEM_PER_PAGE = parseInt(req.body.numItems);
-  res.redirect('back');
+  res.redirect("back");
 };
 
 exports.getSearch = (req, res, next) => {
@@ -195,7 +197,8 @@ exports.getSearch = (req, res, next) => {
     var cart = new Cart(req.session.cart);
     cartProduct = cart.generateArray();
   }
-  searchText = req.query.searchText !== undefined ? req.query.searchText : searchText;
+  searchText =
+    req.query.searchText !== undefined ? req.query.searchText : searchText;
   const page = +req.query.page || 1;
 
   Products.createIndexes({}).catch(err => {
@@ -214,8 +217,8 @@ exports.getSearch = (req, res, next) => {
         .limit(12);
     })
     .then(products => {
-      res.render('search-result', {
-        title: 'Kết quả tìm kiếm cho ' + searchText,
+      res.render("search-result", {
+        title: "Kết quả tìm kiếm cho " + searchText,
         user: req.user,
         searchProducts: products,
         searchT: searchText,
@@ -236,7 +239,7 @@ exports.getSearch = (req, res, next) => {
 exports.postComment = (req, res, next) => {
   const prodId = req.params.productId;
   var tname;
-  if (typeof req.user === 'undefined') {
+  if (typeof req.user === "undefined") {
     tname = req.body.inputName;
   } else {
     tname = req.user.username;
@@ -255,7 +258,7 @@ exports.postComment = (req, res, next) => {
     product.comment.total++;
     product.save();
   });
-  res.redirect('back');
+  res.redirect("back");
 };
 
 exports.getCart = (req, res, next) => {
@@ -266,8 +269,8 @@ exports.getCart = (req, res, next) => {
     var cart = new Cart(req.session.cart);
     cartProduct = cart.generateArray();
   }
-  res.render('shopping-cart', {
-    title: 'Giỏ hàng',
+  res.render("shopping-cart", {
+    title: "Giỏ hàng",
     user: req.user,
     cartProduct: cartProduct
   });
@@ -278,7 +281,7 @@ exports.addToCart = (req, res, next) => {
   var cart = new Cart(req.session.cart ? req.session.cart : {});
   Products.findById(prodId, (err, product) => {
     if (err) {
-      return res.redirect('back');
+      return res.redirect("back");
     }
     cart.add(product, prodId);
     req.session.cart = cart;
@@ -286,7 +289,7 @@ exports.addToCart = (req, res, next) => {
       req.user.cart = cart;
       req.user.save();
     }
-    res.redirect('back');
+    res.redirect("back");
   });
 };
 
@@ -294,12 +297,12 @@ exports.modifyCart = (req, res, next) => {
   var prodId = req.query.id;
   var qty = req.query.qty;
   if (qty == 0) {
-    return res.redirect('back');
+    return res.redirect("back");
   }
   var cart = new Cart(req.session.cart ? req.session.cart : {});
   Products.findById(prodId, (err, product) => {
     if (err) {
-      return res.redirect('back');
+      return res.redirect("back");
     }
     cart.changeQty(product, prodId, qty);
     req.session.cart = cart;
@@ -307,7 +310,7 @@ exports.modifyCart = (req, res, next) => {
       req.user.cart = cart;
       req.user.save();
     }
-    res.redirect('back');
+    res.redirect("back");
   });
 };
 
@@ -317,7 +320,7 @@ exports.getDeleteCart = (req, res, next) => {
     req.user.cart = {};
     req.user.save();
   }
-  res.redirect('back');
+  res.redirect("back");
 };
 
 exports.getDeleteItem = (req, res, next) => {
@@ -325,7 +328,7 @@ exports.getDeleteItem = (req, res, next) => {
   var cart = new Cart(req.session.cart ? req.session.cart : {});
   Products.findById(prodId, (err, product) => {
     if (err) {
-      return res.redirect('back');
+      return res.redirect("back");
     }
     cart.deleteItem(prodId);
     req.session.cart = cart;
@@ -334,7 +337,7 @@ exports.getDeleteItem = (req, res, next) => {
       req.user.save();
     }
     console.log(req.session.cart);
-    res.redirect('back');
+    res.redirect("back");
   });
 };
 
@@ -346,8 +349,8 @@ exports.addOrder = (req, res, next) => {
     var cart = new Cart(req.session.cart);
     cartProduct = cart.generateArray();
   }
-  res.render('add-address', {
-    title: 'Thông tin giao hàng',
+  res.render("add-address", {
+    title: "Thông tin giao hàng",
     user: req.user,
     cartProduct: cartProduct
   });
@@ -373,26 +376,25 @@ exports.postAddOrder = async (req, res, next) => {
     }
 
     order.save((err, result) => {
-      req.flash('success', 'Thanh toán thành công!');
+      req.flash("success", "Thanh toán thành công!");
       req.session.cart = null;
       req.user.cart = {};
       req.user.save();
-      res.redirect('/account');
+      res.redirect("/account");
     });
   } else {
-    req.flash('error', 'Giỏ hàng rỗng!');
-    res.redirect('/account');
+    req.flash("error", "Giỏ hàng rỗng!");
+    res.redirect("/account");
   }
 };
 
 exports.mergeCart = (req, res, next) => {
-  if (req.user.cart != {}) {
-    console.log('alo');
+  if (req.user.cart != {} && req.user.cart) {
     var cart = new Cart(req.session.cart ? req.session.cart : {});
     cart = cart.addCart(req.user.cart);
     req.session.cart = cart;
     req.user.cart = cart;
     req.user.save();
   }
-  res.redirect('/');
+  res.redirect("/");
 };
